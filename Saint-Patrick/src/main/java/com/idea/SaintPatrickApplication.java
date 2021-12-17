cc36a643ebfe2bb60a985713df8c19a8ec09199f
ivan giannini <ivangiannini93@gmail.com>
2021-12-16 21:48:00 -0300

    configuracion bd

--- Saint-Patrick/src/main/java/com/idea/SaintPatrickApplication.java
+++ Saint-Patrick/src/main/java/com/idea/SaintPatrickApplication.java
@@ -1,5 +1,6 @@
 package com.idea;
 
+import com.idea.SaintPatrickApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
