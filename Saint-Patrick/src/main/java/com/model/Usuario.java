53368da50b7c6d6439dd1a16f661559d2ee2b925
MatiBalbiDev <matiasbalbi96@gmail.com>
2022-01-13 11:31:02 -0300

    Agregando atributos y metodos

--- Saint-Patrick/src/main/java/com/model/Usuario.java
+++ Saint-Patrick/src/main/java/com/model/Usuario.java
@@ -5,5 +5,53 @@ public class Usuario {
 	private Integer id;
 	private String nombre;
 	private String apellido;
-	private String telefono;
+	private String email;
+	private String nroTarjeta;
+	private String contraseña;
+	
+	public Usuario() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNroTarjeta() {
+		return nroTarjeta;
+	}
+
+	public void setNroTarjeta(String nroTarjeta) {
+		this.nroTarjeta = nroTarjeta;
+	}
+	
+	
 }
