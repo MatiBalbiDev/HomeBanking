ed42ba57c1edb2c302cb5af65da2ce1257d082a6
MatiBalbiDev <matiasbalbi96@gmail.com>
2022-01-17 23:44:50 -0300

    Creacion de un repositorio JPA

---
+++ Saint-Patrick/src/main/java/com/idea/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.idea.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+import com.idea.model.Usuario;
+
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
+	
+}
